a6ffbc133299a62ecf2e547ec306a62a6a3b291e
sergioga06 <sergiogallardog06@gmail.com>
2025-12-05 13:42:25 +0100

    docker-users

--- apps/ms-usuarios/src/usuarios/usuarios.service.ts
+++ apps/ms-usuarios/src/usuarios/usuarios.service.ts
@@ -1,39 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service'; // Importa tu servicio
-import { CreateUserDto } from '@app/common'; // Tus DTOs compartidos
-import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from '../../../../libs/common/src/dto/ms-usuarios/create-usuario.dto';
+import { UpdateUserDto } from '../../../../libs/common/src/dto/ms-usuarios/update-usuario.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
-
-  async create(createUserDto: CreateUserDto) {
-    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
-    
-    // Aquí asignamos roles. Podrías recibir los IDs de roles en el DTO
-    return this.prisma.user.create({
-      data: {
-        ...createUserDto,
-        password: hashedPassword,
-        // Ejemplo: Asignar rol por defecto o el que venga en el DTO
-        roles: {
-          connect: { slug: 'waiter' } // Conectar por slug existente
-        }
-      },
-      include: { roles: true } // Para devolver el usuario con sus roles
-    });
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
   }
 
-  async findOne(username: string) {
-    return this.prisma.user.findUnique({
-      where: { username },
-      include: {
-        roles: {
-          include: { permisos: true } // Traemos roles y sus permisos
-        }
-      }
-    });
+  findAll() {
+    return `This action returns all users`;
   }
-  
-  // ... resto de métodos (update, delete)
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
 }
