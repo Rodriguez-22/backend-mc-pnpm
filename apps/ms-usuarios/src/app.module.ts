3545505dacff8b846b52912344a8fa067191fc47
sergioga06 <sergiogallardog06@gmail.com>
2025-12-06 15:32:44 +0100

    Subir arriba para poder trabajar en la torre

--- apps/ms-usuarios/src/app.module.ts
+++ apps/ms-usuarios/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
 import { PermisosModule } from './permisos/permisos.module';
 
 @Module({
   imports: [RolesModule, PermisosModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
