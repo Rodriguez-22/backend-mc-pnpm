499a227fa6ec2548adecfd8c298e16c5fbcd0a34
Rodriguez-22 <jrodrod594@g.educaand.es>
2025-12-11 12:45:27 +0100

    solucionando errores para el despliegue del gateway

--- apps/ms-client-gateway/deploy/start.sh
+++ apps/ms-client-gateway/deploy/start.sh
@@ -1,12 +1,8 @@
 set -e
 
-main() {
-  # Ejecutar el microservicio Client Gateway en modo desarrollo
-  # Se asume que el script 'start:client-gateway:dev' está definido
-  # en el package.json principal.
-  pnpm start:gateway:dev
-}
+pnpm start:gateway:dev
 
-main
-# Se elimina 'tail -f /dev/null' ya que la aplicación NestJS
-# debe ser el proceso principal (PID 1) del contenedor.
+pm2 start dist/apps/gateway/main.js --name "gateway" --no-daemon
+
+pm2 logs gateway
+
