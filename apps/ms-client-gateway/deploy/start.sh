092673087994f6cda4b901e6d7402b332bc50763
Rodriguez-22 <jrodrod594@g.educaand.es>
2025-12-11 14:16:35 +0100

    soluciones

--- apps/ms-client-gateway/deploy/start.sh
+++ apps/ms-client-gateway/deploy/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
+
+pnpm run build:gateway
+
 pm2 start dist/apps/ms-client-gateway/main.js --name "gateway" --no-daemon
 
-pnpm start:gateway:dev
+pnpm start:prod:gateway
 
 pm2 logs gateway
 
