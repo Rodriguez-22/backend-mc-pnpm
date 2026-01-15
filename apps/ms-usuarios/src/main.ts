9dcaa70602afcb12705709dc06a376fe0bf4f8dd
jose <jrodrod594@g.educaand.es>
2026-01-15 16:08:24 +0100

    mas soluciones

--- apps/ms-usuarios/src/main.ts
+++ apps/ms-usuarios/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
-import { envs } from 'apps/ms-client-gateway/src/config/envs';
+import { envs } from '../config/envs';
 
 async function bootstrap() {
  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
