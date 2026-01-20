a97c995a8b81fa0a333d8220147c3fc657ae4abe
sergioga06 <sergiogallardog06@gmail.com>
2026-01-20 10:32:00 +0100

    conexiones por cors

--- apps/ms-client-gateway/src/main.ts
+++ apps/ms-client-gateway/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
+  const logger = new Logger('Gateway-Main');
+
+  // 👇 ESTA ES LA PIEZA QUE FALTA 👇
+  app.enableCors({
+    // Permitimos específicamente tu frontend local
+    origin: ['http://localhost:3001', 'http://localhost:3000'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  // Configuración de validaciones (opcional pero recomendado)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(3000);
+  logger.log('🚀 Gateway escuchando peticiones en el puerto 3000');
 }
 bootstrap();
