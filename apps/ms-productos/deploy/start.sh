aab1aa7fc393dee46e7cb78e81112ba9d798966c
sergioga06 <sergiogallardog06@gmail.com>
2026-02-11 08:47:45 +0100

    despliegue productos

--- apps/ms-productos/deploy/start.sh
+++ apps/ms-productos/deploy/start.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-set -e
-
-main(){
-    # Instalación de herramientas
-    npm install -g npm@11.7.0
-    npm install -g pnpm
-    npm install -g pm2
-
-    # Instalar dependencias
-    pnpm install --frozen-lockfile
-
-    echo "Iniciando Microservicio Productos..."
-
-    # Borrar proceso antiguo si existe y arrancar
-    pm2 delete ms-productos 2>/dev/null || true
-    
-    # IMPORTANTE: Solo arrancar, SIN líneas de Prisma
-    pm2 start pnpm --name "ms-productos" -- run start:dev ms-productos
-}
-main
-tail -f /dev/null
+# Como usas TypeORM con synchronize:true, no necesitamos el comando de migrate de Prisma
+node dist/apps/ms-productos/main.js
